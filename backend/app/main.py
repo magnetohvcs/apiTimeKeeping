1f119b69ebfcd111b9c88389c1ff8227cdea1fa7
magnetohvcs <ttrungt1742@gmail.com>
2022-05-12 21:11:51 +0700

    update api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, jsonify
 import db
 
@@ -12,6 +13,44 @@ def index():
 def getEmployee():
     data = db.getEmployee()
     return data
+
+@app.route('/employee/delEmployee', methods=['POST'])
+def delEmployee():
+    return jsonify({"message" : db.delEmployee(request.json)})
+
+@app.route('/employee/addEmployee', methods=['POST'])
+def addEmployee():
+    try:
+        return jsonify({"message" : db.addEmployee(request.json['firstName'], request.json['lastName'])})
+    except:
+        return jsonify({"message" : False})
+
+
+@app.route('/product/getProduct')
+def getProduct():
+    data = db.getProduct()
+    return data
+
+@app.route('/product/addProduct', methods=['POST'])
+def addProduct():
+    try:
+        db.addProduct(request.json['name'], request.json['price'])
+        return jsonify({"message" : True})
+    except:
+        return jsonify({"message" : False})
+
+@app.route('/product/delProduct', methods=['POST'])
+def delProduct():
+    return jsonify({"message" : db.delProduct(request.json)})
+
+@app.route('/product/editProduct', methods=['POST'])
+def editProduct():
+    try:
+        db.editProduct(request.json)
+        return jsonify({"message" : True})
+    except:
+        return jsonify({"message" : False})
+
 
 if __name__=="__main__":
     app.run("0.0.0.0",8080,True)
